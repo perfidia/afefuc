1b17fce35f37d11462ed73013c95a70fe9cc71a5
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2013-05-12 02:38:02 +0200

    support for Ctrl+C

--- src/afefuc.py
+++ src/afefuc.py
@@ -7,14 +7,16 @@ Created on Apr 25, 2013
 '''
 
 import sys
+import signal
 
 sys.path.append('../src-ui')
 
 from PyQt4 import QtCore, QtGui
-
 from gui.MainWindowWrapper import MainWindowWrapper
 
 if __name__ == "__main__":
+	signal.signal(signal.SIGINT, signal.SIG_DFL)
+
 	app = QtGui.QApplication(sys.argv)
 	myapp = MainWindowWrapper()
 	myapp.show()
