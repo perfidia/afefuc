a3b4ae874d5f1fe1498dc64ae6f407e65b3b24d1
mkleinert <sprinter31>
2013-06-05 21:26:58 +0200

    move README informations to mm2xml.py header

--- src/utils/mm2xml.py
+++ src/utils/mm2xml.py
@@ -1,6 +1,29 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+This script converts .mm files (current format for FreeMind) to XML format
+
+How to use:
+
+./mm2xml.py [input file] [output file] 
+
+Output XML file structure:
+
+main element -> [start]
+ children elements:
+  actor
+  text
+  action -> has attributes:
+   action 
+  value 
+  name 
+  url 
+  object -> has attributes:
+   type 
+  number
+'''
+
 from os import path
 from sys import argv, exit
 from xml.dom import minidom
